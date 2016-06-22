6015aa013fd8a0f8d8814e87392c7bd27e3d30d2
Ben <lyenliang@gmail.com>
2016-06-22 09:37:02 +0000

    Refactor with helper

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,9 +1,25 @@
 module StocksHelper
     def render_return_ratio(current_price, enter_price)
-        return ((current_price - enter_price) * 100 / enter_price).round(2)
+        value = ((current_price - enter_price) * 100 / enter_price).round(2)
+        
+        return render_color_text(value, '%')
     end
     
     def render_return_diff(current_price, enter_price)
-        return (current_price - enter_price).round(2)
+        value = (current_price - enter_price).round(2)
+        
+        return render_color_text(value)
+    end 
+    
+    private 
+    
+    def render_color_text(value, suffix='')
+        if value > 0
+            return content_tag(:span, value.to_s + suffix, :class => :red_text)
+        elsif value < 0
+            return content_tag(:span, value.to_s + suffix, :class => :green_text)
+        else 
+            return content_tag(:span, value.to_s + suffix)
+        end
     end
 end
